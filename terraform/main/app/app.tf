8eae0f6b9e4b545fe41c6a699a69bdd4c58ae9f1
Minh Pham <phamminh@google.com>
2026-06-11 19:50:23 +0000

    Fix: Remove root_agent from google_ces_app to fix creation error

--- terraform/main/app/app.tf
+++ terraform/main/app/app.tf
@@ -4,8 +4,6 @@ resource "google_ces_app" "my_app" {
   location     = var.location
   display_name = var.display_name
 
-  root_agent = "projects/${var.project}/locations/${var.location}/apps/${var.app_id}/agents/d716ad6f-3dc3-46f5-8f4b-f3430241e777"
-
   time_zone_settings {
     time_zone = "America/Los_Angeles"
   }
